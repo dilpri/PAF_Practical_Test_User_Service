2ca90d15a0f782eb960b487226533995080db65a
dilpri <dilpripunchihewa@gmail.com>
2020-05-06 17:38:54 +0530

    errors fixed

--- HospitalServicePT/src/com/HospitalsAPI.java
+++ HospitalServicePT/src/com/HospitalsAPI.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/HospitalsAPI")
 public class HospitalsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Hospital hospitalObj = new Hospital();
